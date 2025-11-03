12681fa7230c8950f6df832e73b748d7015c65fc
Neha <130530380+nehaaaah123@users.noreply.github.com>
2025-11-03 10:36:27 +0530

    Update Program.cs

--- backend/Ecommerce.Api/Program.cs
+++ backend/Ecommerce.Api/Program.cs
@@ -4,7 +4,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Ecommerce.Api.Data;
-using Ecommerce.Api.Repositories;//here im connecting sql server
+using Ecommerce.Api.Repositories;
 
 var builder = WebApplication.CreateBuilder(args); 
 
